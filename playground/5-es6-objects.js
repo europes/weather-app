0837e53665ccbb48753d5fec99f6ae8400f3bf3a
europes <64372763+europes@users.noreply.github.com>
2021-04-23 19:30:21 +0100

    update for herkou2

---
+++ playground/5-es6-objects.js
@@ -0,0 +1,34 @@
+const name = 'andrew'
+const userAge = 27
+
+const user = {
+   name,
+    age: userAge,
+    location: 'Philadelphia'
+}
+
+console.log(user)
+
+// object destructuring
+
+const product = {
+    label: 'red notebook',
+    price: 3,
+    stock: 201,
+    salePrice: undefined,
+    rating: 4.2
+}
+
+// const label = product.label
+// const stock = product.stock
+
+// const {label:productLabel, stock, rating = 5} = product 
+// console.log (productLabel)
+// console.log(stock)
+// console.log(rating)
+
+const transaction = (type, {label, stock = 0 } = {}) => {
+console.log(type, label, stock)
+}
+
+transaction('order', product)
